98c479b023f56b90eff1af70668305f6045f7f0f
Rob <rob@bladen.me>
2021-03-20 15:15:47 +0000

    Setup webpack production. Add runtime compilation.

--- Startup.cs
+++ Startup.cs
@@ -8,14 +8,18 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Quiz.Webpack;
 
 namespace Quiz
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IWebHostEnvironment _env;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
-            Configuration = configuration;
+            Configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            _env = env ?? throw new ArgumentNullException(nameof(env));
         }
 
         public IConfiguration Configuration { get; }
@@ -23,7 +27,10 @@ namespace Quiz
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+
+            if (!_env.IsDevelopment())
+                WebpackChunkNamer.Init();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

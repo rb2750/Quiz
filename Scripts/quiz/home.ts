06329e86cd806f5a54d1b9ad6d65fbbd334630e7
Rob <rob@bladen.me>
2021-03-21 23:20:20 +0000

    Front end javascript for home functionality.

--- Scripts/quiz/home.ts
+++ Scripts/quiz/home.ts
@@ -1,4 +1,87 @@
+import Swal from 'sweetalert2';
+
 export default class Home {
 	constructor() {
+		this.addDeleteButtonHandler();
+		this.addCreateButtonHandler();
+	}
+
+	private addDeleteButtonHandler(): void {
+		$(document).on('click', '#deleteQuiz', function () {
+			// Fetch the quiz ID from the element
+			const quizId = $(this).data('quizId');
+
+			$.ajax('/DeleteQuiz', {
+				data: {
+					__RequestVerificationToken: $(
+						'input[name="__RequestVerificationToken"]'
+					).val() /* Verify the request using an ASP token */,
+					quizId
+				},
+				method: 'post',
+				success(response) {
+					if (response.success) {
+						Swal.fire({
+							title: 'Success',
+							text: 'Quiz deleted successfully',
+							icon: 'success',
+							onClose: () => document.location.reload()
+						});
+					} else {
+						Swal.fire({ title: 'Error', text: response.message, icon: 'error' });
+					}
+				},
+				error() {
+					Swal.fire({ title: 'Error', text: 'There was an error in sending the request.', icon: 'error' });
+				}
+			});
+		});
+	}
+
+	private static async showQuizNameDialog(): Promise<string> {
+		const { value: quizName } = await Swal.fire({
+			title: 'Enter the new quiz name',
+			input: 'text',
+			inputPlaceholder: 'Quiz name',
+			showCancelButton: true
+		});
+
+		return quizName;
+	}
+
+	private addCreateButtonHandler(): void {
+		$(document).on('click', '#addQuiz', () => {
+			Home.showQuizNameDialog().then((quizName: string) => {
+				// If they didn't press cancel.
+				if (quizName !== undefined) {
+					$.ajax('/CreateQuiz', {
+						data: {
+							__RequestVerificationToken: $(
+								'input[name="__RequestVerificationToken"]'
+							).val() /* Verify the request using an ASP token */,
+							name: quizName
+						},
+						method: 'post',
+						success(response) {
+							if (response.success) {
+								Swal.fire({
+									title: 'Success',
+									text: 'Quiz created successfully',
+									icon: 'success',
+									onClose: () => {
+										document.location.href = response.redirect;
+									}
+								});
+							} else {
+								Swal.fire({ title: 'Error', text: response.message, icon: 'error' });
+							}
+						},
+						error() {
+							Swal.fire({ title: 'Error', text: 'There was an error in sending the request.', icon: 'error' });
+						}
+					});
+				}
+			});
+		});
 	}
 }

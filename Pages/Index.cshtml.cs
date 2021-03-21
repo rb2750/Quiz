b43ea38f32c5ed37fdd50854584779cb81cbef94
Rob <rob@bladen.me>
2021-03-21 17:38:40 +0000

    Added backend for fetching quizzes to display to the user.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,24 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Quiz.Context;
 
 namespace Quiz.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly QuizDbContext _context;
+        public List<Models.Quiz> quizzes = new();
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, QuizDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public void OnGet()
         {
+            quizzes = _context.Quizzes.Include(quiz => quiz.Responses).ToList();
         }
     }
 }

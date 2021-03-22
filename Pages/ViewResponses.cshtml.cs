068a20aa9d72900b9f417a52257e12992c553643
Rob <rob@bladen.me>
2021-03-22 12:44:00 +0000

    Created a view responses page

---
+++ Pages/ViewResponses.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Quiz.Context;
+
+namespace Quiz.Pages
+{
+    public class ViewResponses : PageModel
+    {
+        private readonly QuizDbContext _context;
+        public Models.Quiz quiz;
+
+        public ViewResponses(QuizDbContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet(int quizId)
+        {
+            quiz = _context.Quizzes.Include(q => q.Responses).FirstOrDefault(q => q.ID == quizId);
+        }
+    }
+}

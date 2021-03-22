c7534f959b68a908e0d9ac3c6fbfaef4c1210785
Rob <rob@bladen.me>
2021-03-22 12:44:37 +0000

    Updated the quiz page to be able to display a quiz response.

--- Pages/Quiz.cshtml.cs
+++ Pages/Quiz.cshtml.cs
@@ -16,15 +16,36 @@ namespace Quiz.Pages
     {
         private readonly QuizDbContext _context;
         public Models.Quiz quiz;
+        public Models.QuizResponse response;
 
         public QuizModel(QuizDbContext context)
         {
             _context = context;
         }
 
-        public void OnGet(int quizId)
+        public void OnGet(int quizId, int? quizResponseId)
         {
             quiz = _context.Quizzes.Include(q => q.Questions).ThenInclude(q => q.Answers).FirstOrDefault(q => q.ID == quizId);
+
+            if (quizResponseId != null) response = _context.QuizResponses.Include(qr => qr.Answers).FirstOrDefault(r => r.ID == quizResponseId);
+        }
+
+        public JsonResult OnPostSubmitQuiz(int quizId, List<QuizResponseItem> answers)
+        {
+            int changesMade = 0;
+
+            var quizResponse = new QuizResponse {QuizID = quizId};
+
+            _context.QuizResponses.Add(quizResponse);
+            changesMade += _context.SaveChanges();
+
+            answers.ForEach(answer => answer.QuizResponseId = quizResponse.ID);
+
+            //Insert all the responses into the database.
+            _context.QuizResponseItems.AddRange(answers);
+            changesMade += _context.SaveChanges();
+
+            return new JsonResult(new {success = changesMade > 0, message = changesMade > 0 ? null : "There was an error modifying the database."});
         }
     }
 }
--- Scripts/quiz/quiz.ts
+++ Scripts/quiz/quiz.ts
@@ -2,19 +2,62 @@ import Swal from 'sweetalert2';
 
 export default class Quiz {
 	constructor() {
-		this.addAnswerClickHandler();
+		// Only add the click handler if the page isn't being viewed.
+		if (!$('#quiz-question-list').hasClass('viewing')) this.addAnswerClickHandler();
 		this.addSaveClickHandler();
 	}
 
 	private addAnswerClickHandler(): void {
-		$(document).on('click', '#answer-container a.list-group-item', () => {
-			$(this).toggleClass('active');
+		$(document).on('click', '#answer-container a.list-group-item', (data) => {
+			$(data.target).toggleClass('active');
 		});
 	}
 
 	private addSaveClickHandler(): void {
+		const quizId = new URL(window.location.href).searchParams.get('quizId');
 		$(document).on('click', '#save-quiz', () => {
-			Swal.fire({ title: 'Error', text: 'This feature has not yet been implemented.', icon: 'error' });
+			$.ajax('/Quiz/SubmitQuiz', {
+				data: {
+					__RequestVerificationToken: $(
+						'input[name="__RequestVerificationToken"]'
+					).val() /* Verify the request using an ASP token */,
+					quizId,
+					answers: this.getQuizAnswersFromPage()
+				},
+				method: 'post',
+				success(response) {
+					if (response.success) {
+						Swal.fire({
+							title: 'Success',
+							text: 'Quiz submitted successfully',
+							icon: 'success',
+							onClose: () => {
+								document.location.href = '/';
+							}
+						});
+					} else {
+						Swal.fire({ title: 'Error', text: response.message, icon: 'error' });
+					}
+				},
+				error() {
+					Swal.fire({ title: 'Error', text: 'There was an error in sending the request.', icon: 'error' });
+				}
+			});
 		});
+	}
+
+	private getQuizAnswersFromPage(): Array<{ Question: number; Answer: number }> {
+		const answers: Array<{ Question: number; Answer: number }> = [];
+
+		// Find all the selected (active) answers
+		for (const answer of $('#answer-container a.list-group-item.active')) {
+			// Retrieve the question and answer IDs stored on the element.
+			const questionId = $(answer).data('questionId');
+			const answerId = $(answer).data('answerId');
+
+			answers.push({ Question: questionId, Answer: answerId });
+		}
+
+		return answers;
 	}
 }
